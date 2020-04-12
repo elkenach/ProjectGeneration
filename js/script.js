9ff66210de36ed3de69cc112703751caa034ca77
Elena <elenachavez36@gmail.com>
2020-04-12 15:26:35 -0500

    profile

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
-let nav = document.getElementById('nav');
-let menu = document.getElementById('enlaces');
-let open = document.getElementById('open');
-let buttons = document.getElementsByClassName('btn-header');
+let nav = document.getElementById("nav");
+let menu = document.getElementById("enlaces");
+let open = document.getElementById("open");
+let buttons = document.getElementsByClassName("btn-header");
 let close = true;
 
 
@@ -27,11 +27,11 @@ function menus(){
 
 function opening(){
     if(close){
-        menu.style.width = '45vw';
+        menu.style.width = "45vw";
         close = false;
     }else{
-        menu.style.width = '0%';
-        menu.style.overflow = 'hidden';
+        menu.style.width = "0%";
+        menu.style.overflow = "hidden";
         close = true;
     }
 }
@@ -68,4 +68,45 @@ window.addEventListener("resize", function(){
 open.addEventListener("click", function(){
 	opening();
 });
+
+
+
+
+/* Filtro botones dash*/
+
+$(function () {
+    $(".filter").click(function () {
+        $(this).addClass("active").siblings().removeClass("active");
+        let valor =  $(this).attr("data-nombre");
+        if(valor == "todos"){
+        	$(".content-post").show("1000");	
+        }else{
+        	$(".content-post").not("."+ valor).hide("1000");
+        	$(".content-post").filter("."+ valor).show("1000");
+        }        
+    });
+
+
+
+/* Enlaces */
+
+
+    window.addEventListener("resize", function(){
+        let home = $("#home").offset().top;
+        
+        
+    });
+
+    $("#enlace-home").on("click", function(e){
+        e.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        },600);
+    });
+
+  
+});
+
+
+
 
